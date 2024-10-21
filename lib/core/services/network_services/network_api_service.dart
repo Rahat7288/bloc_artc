97c146ea39247e2864f5cbae2a0ac3b840bc3053
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-10-21 17:11:15 +0600

    get api network service added

--- lib/core/services/network_services/network_api_service.dart
+++ lib/core/services/network_services/network_api_service.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
 import 'package:bloc_arct/core/constants/app_urls.dart';
+import 'package:bloc_arct/core/resources/data_state.dart';
 import 'package:bloc_arct/core/services/network_services/base_api_services.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/src/widgets/framework.dart';
+
+import 'app_exceptions.dart';
 
 class NetworkApiService extends BaseApiServices {
   static Future<BaseOptions> getBaseOptions(
@@ -24,12 +29,7 @@ class NetworkApiService extends BaseApiServices {
     return baseOptions;
   }
 
-  @override
-  Future<dynamic> deleteApi(payload, BuildContext context) {
-    // TODO: implement deleteApi
-    throw UnimplementedError();
-  }
-
+  // network API service for get data response type dynamic
   @override
   Future<dynamic> getApi(String url, data, BuildContext context) async {
     if (kDebugMode) {
@@ -45,8 +45,14 @@ class NetworkApiService extends BaseApiServices {
         print(response.statusCode);
         print(response.data);
       }
-    } catch (e) {}
-    throw UnimplementedError();
+
+      responseJson = returResponse(response, context);
+    } on DioException catch (e) {
+      throw ('Exception=========== $e');
+    } on SocketException {
+      throw InternetException();
+    }
+    return responseJson;
   }
 
   @override
@@ -59,5 +65,34 @@ class NetworkApiService extends BaseApiServices {
   Future<dynamic> updateApi(payload, url, BuildContext context) {
     // TODO: implement updateApi
     throw UnimplementedError();
+  }
+
+  @override
+  Future<dynamic> deleteApi(payload, BuildContext context) {
+    // TODO: implement deleteApi
+    throw UnimplementedError();
+  }
+
+  dynamic returResponse(Response response, context) async {
+    switch (response.statusCode) {
+      case 200:
+        dynamic responseJson = response.data;
+        return DataSuccess(responseJson);
+      case 201:
+        dynamic responseJson = response.data;
+        return responseJson;
+      case 400:
+        dynamic responseJson = response.data;
+        return responseJson;
+
+      case 500:
+        dynamic responseJson = response.data;
+        return DataFailed(responseJson);
+
+      default:
+        throw FetchDataExceptions(
+            'Error accorded while communicating with server' +
+                response.statusCode.toString());
+    }
   }
 }
